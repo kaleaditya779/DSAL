ba24adc9d2b86f567c081d442fc968e66213e392
Aditya <kaleaditya779@gmail.com>
2023-06-06 18:31:22 +0530

    setADT Set2 input+display added

--- Sachin_DSAL/1_set_ADT.py
+++ Sachin_DSAL/1_set_ADT.py
@@ -55,14 +55,17 @@ def menu():
     while True:
         print("==== Set Operations Menu ====")
         print("1. Add element to Set 1")
-        print("2. Remove element from Set 1")
-        print("3. Check if element is in Set 1")
-        print("4. Get size of Set 1")
-        print("5. Perform intersection of Set 1 and Set 2")
-        print("6. Perform union of Set 1 and Set 2")
-        print("7. Get difference between Set 1 and Set 2")
-        print("8. Check if Set 1 is a subset of Set 2")
-        print("9. Exit")
+        print("2. Add element to Set 2")
+        print("3. Remove element from Set 1")
+        print("4. Check if element is in Set 1")
+        print("5. Get size of Set 1")
+        print("6. Perform intersection of Set 1 and Set 2")
+        print("7. Perform union of Set 1 and Set 2")
+        print("8. Get difference between Set 1 and Set 2")
+        print("9. Check if Set 1 is a subset of Set 2")
+        print("10. Display Set 1")
+        print("11. Display Set 2")
+        print("12. Exit")
 
         choice = int(input("Enter your choice: "))
 
@@ -71,32 +74,40 @@ def menu():
             set_1.add(element)
             print("Element added to Set 1.")
         elif choice == 2:
+            element = input("Enter the element to add: ")
+            set_2.add(element)
+            print("Element added to Set 2.")
+        elif choice == 3:
             element = input("Enter the element to remove: ")
             set_1.remove(element)
             print("Element removed from Set 1.")
-        elif choice == 3:
+        elif choice == 4:
             element = input("Enter the element to check: ")
             if set_1.contains(element):
                 print("Element is in Set 1.")
             else:
                 print("Element is not in Set 1.")
-        elif choice == 4:
-            print("Size of Set 1 is:", set_1.size())
         elif choice == 5:
+            print("Size of Set 1 is:", set_1.size())
+        elif choice == 6:
             intersection = set_1.intersection(set_2)
             print("Intersection of Set 1 and Set 2:", intersection.elements)
-        elif choice == 6:
+        elif choice == 7:
             union = set_1.union(set_2)
             print("Union of Set 1 and Set 2:", union.elements)
-        elif choice == 7:
+        elif choice == 8:
             difference = set_1.difference(set_2)
             print("Difference between Set 1 and Set 2:", difference.elements)
-        elif choice == 8:
+        elif choice == 9:
             if set_1.subset(set_2):
                 print("Set 1 is a subset of Set 2.")
             else:
                 print("Set 1 is not a subset of Set 2.")
-        elif choice == 9:
+        elif choice == 10:
+            print(set_1.elements)
+        elif choice == 11:
+            print(set_2.elements)
+        elif choice == 12:
             print("Exiting the program...")
             break
         else:
